24811030c150710f3d4d379a57db276e82b5c8cd
Makis Chartalos <makischartalos@gmail.com>
2024-04-01 14:26:25 +0300

    component input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PersonTableComponent } from './components/person-table/person-table.component';
+import { Person } from './shared/interfaces/person';
 
 
 @Component({
@@ -12,5 +13,22 @@ import { PersonTableComponent } from './components/person-table/person-table.com
 export class AppComponent {
   name = 'Chartalos';
   
+  person0: Person =
+{
+    givenName: "Makis",
+    surName: "Chartalos",
+    age: 0x37,
+    email: 'makischartalos@gmail.com',
+    address: "Athens, Greece"
+  };
+
+  person1: Person =
+  {
+      givenName: "John",
+      surName: "Doe",
+      age: 0x25,
+      email: 'joe@example.com',
+      address: "New York, "
+    }
 
 }
--- src/app/components/person-table/person-table.component.ts
+++ src/app/components/person-table/person-table.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Person } from 'src/app/shared/interfaces/person';
+
 
 @Component({
   selector: 'app-person-table',
@@ -8,11 +10,5 @@ import { Component } from '@angular/core';
   styleUrl: './person-table.component.css'
 })
 export class PersonTableComponent {
-  person = {
-    givenName: "Makis",
-    surName: "Chartalos",
-    age: 0x37,
-    email: 'makischartalos@gmail.com'
-  }
-
+  @Input() person: Person  | undefined; 
 }
